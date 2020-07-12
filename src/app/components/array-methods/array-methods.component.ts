e0fd783a9a04964e796fbc0f024489b2fc86824e
Ranaivoson <air_manitra@yahoo.fr>
2020-07-12 03:58:32 +0300

    addText-createVisual

--- src/app/components/array-methods/array-methods.component.ts
+++ src/app/components/array-methods/array-methods.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import * as d3 from "d3";
-export const MAX_ELEMENTS = 18;
+export interface circleData {
+  x_axis: number;
+  y_axis: number;
+  radius: number;
+  color: string;
+  textIndex: number;
+};
 @Component({
   selector: 'app-array-methods',
   templateUrl: './array-methods.component.html',
@@ -9,43 +15,62 @@ export const MAX_ELEMENTS = 18;
 
 export class ArrayMethodsComponent implements OnInit {
   public searchKey: number;
-  public svg1: any;
-  public svg2: any;
-  public svg3: any;
-
   constructor() { }
 
   ngOnInit(): void {
-    this.initVisual();
+    this.initializeVisual();
   }
 
-  initVisual() {
-    this.svg1 = d3.select("#arrayMethodVisual-col1").append("svg").attr("width", 300).attr("height", 450);
-    this.svg2 = d3.select("#arrayMethodVisual-col2").append("svg").attr("width", 300).attr("height", 450);
-    this.svg3 = d3.select("#arrayMethodVisual-col3").append("svg").attr("width", 300).attr("height", 450);
-    [this.svg1, this.svg2, this.svg3].forEach((svgElement:any, index: number) => {
-      this.createVisual(svgElement, index*50);
+  initializeVisual() {
+    ['#arrayMethodVisual-col1', '#arrayMethodVisual-col2', '#arrayMethodVisual-col3'].forEach((Id:string, index: number) => {
+      this.createVisual(Id, index*50);
     });
   }
 
-  private createVisual(svg: any, index: number) {
-    const jsonCircles = [
-      { "x_axis": 50 + index, "y_axis": 400, "radius": 20, "color" : "#69a3b2" },
-      { "x_axis": 50 + index, "y_axis": 350, "radius": 20, "color" : "#69a3b2"},
-      { "x_axis": 50 + index, "y_axis": 300, "radius": 20, "color" : "#69a3b2"},
-      { "x_axis": 50 + index, "y_axis": 250, "radius": 20, "color" : "#69a3b2" },
-      { "x_axis": 50 + index, "y_axis": 200, "radius": 20, "color" : "#69a3b2"},
-      { "x_axis": 50 + index, "y_axis": 150, "radius": 20, "color" : "#69a3b2"}];
-    const circles = svg.selectAll("circle")
-                               .data(jsonCircles)
-                               .enter()
-                               .append("circle");
-    const circleAttributes = circles
-                          .attr("cx", function (d) { return d.x_axis; })
-                          .attr("cy", function (d) { return d.y_axis; })
-                          .attr("r",  function (d) { return d.radius; })
-                          .attr('stroke', 'black')
-                          .style("fill", function(d) { return d.color; });
+  private createVisual(Id: string, index: number) {
+    const svg: any = d3.select(Id).append("svg").attr("width", 300).attr("height", 450);
+    const jsonCircles: circleData[] = [
+    { x_axis: 50 + index, y_axis: 400, radius: 20, color : "#69a3b2", textIndex: 5},
+    { x_axis: 50 + index, y_axis: 350, radius: 20, color : "#69a3b2", textIndex: 4},
+    { x_axis: 50 + index, y_axis: 300, radius: 20, color : "#69a3b2", textIndex: 3},
+    { x_axis: 50 + index, y_axis: 250, radius: 20, color : "#69a3b2" , textIndex: 2},
+    { x_axis: 50 + index, y_axis: 200, radius: 20, color : "#69a3b2", textIndex: 1},
+    { x_axis: 50 + index, y_axis: 150, radius: 20, color : "#69a3b2", textIndex: 0}
+  ];
+  if (index/50 === 1) {
+    let k: number = 12
+    jsonCircles.forEach((element:circleData) => {
+      element.textIndex =k;
+      k -=1;
+    });
+  }
+  if (index/50 === 2) {
+    let k: number = 18
+    jsonCircles.forEach((element:circleData) => {
+      element.textIndex =k;
+      k -=1;
+    });
+  }
+  const circles = svg.selectAll("circle")
+                            .data(jsonCircles)
+                            .enter()
+                            .append("circle");
+ const circleAttributes = circles
+                        .attr("cx", (d: circleData) => { return d.x_axis; })
+                        .attr("cy", (d:circleData) => { return d.y_axis; })
+                        .attr("r",  (d:circleData) => { return d.radius; })
+                        .style("fill", (d:circleData) => { return d.color; });
+ const text = svg.selectAll("text")
+                         .data(jsonCircles)
+                         .enter()
+                         .append("text");
+ const textLabels = text
+                  .attr("x", (d:circleData) => { return d.x_axis; })
+                  .attr("y", (d:circleData) => { return d.y_axis; })
+                  .text((d:circleData) => { return d.textIndex; })
+                  .attr("font-family", "sans-serif")
+                  .attr("font-size", "15px")
+                  .attr("fill", "black");
   }
 
   newArray(): void {
